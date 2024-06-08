942a8014d1dda1866363c58dc849c1a0d7c82f3b
Mohamed-khadarr <mohamed.bile@strathmore.edu>
2024-06-08 14:51:44 +0300

    added authentication

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,6 +1,10 @@
-
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
-    <p>This is a protected page</p>
-  );
+    <div>
+      <UserButton
+        afterSignOutUrl="/" 
+      />
+    </div>
+    );
 }
